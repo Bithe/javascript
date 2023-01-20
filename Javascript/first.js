bc33bae0bcc8b8444217f29dfd1793e25ff01b0e
Bithe <farzana.hossain5@gmail.com>
2023-01-20 10:18:43 +0200

    first init

---
+++ Javascript/first.js
@@ -0,0 +1,12 @@
+console.log('heloo');
+const a =10;
+console.log(a);
+let b=20;
+console.log(b);
+
+let c =a+b;
+console.log('c=',c);
+const result= 'c= '+c;
+console.log(result);
+console.log('sum= '+a+b);
+console.log('sum= '+(a+b));
