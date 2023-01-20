75b3d81ec3129b0d3211c79ea88f234d671f4291
Bithe <farzana.hossain5@gmail.com>
2023-01-20 12:50:09 +0200

    random

--- Javascript/random.js
+++ Javascript/random.js
@@ -1,3 +1,25 @@
 const suite=Math.random();
 
-console.log(suite);
+const rank=Math.floor(Math.random()*13)+1;
+
+//console.log(suite);
+
+let suiteSymbol;
+
+if(suite===0){
+    suiteSymbol='\u2665';
+}
+
+else if(suite===1){
+    suiteSymbol='\u2666';
+}
+
+else if(suite===0){
+    suiteSymbol='\u2660';
+}
+
+else{
+    suiteSymbol='\u2663';
+}
+
+console.log(`${suiteSymbol} ${rank}`);
