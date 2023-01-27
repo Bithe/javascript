6698ae7e058e4955b64dd8a67d958b64a582c47d
Bithe <farzana.hossain5@gmail.com>
2023-01-27 10:25:45 +0200

    array up

--- Javascript/arraydemo.js
+++ Javascript/arraydemo.js
@@ -1,5 +1,7 @@
-let resultString='';
+let numbers, resultString='';
 sum=0;
 for(let i=0;i<4;i++){
     sum+=numbers[i];
+    resultString+=numbers[i]+' + ';
 }
+console.log(resultString+' = '+sum);
