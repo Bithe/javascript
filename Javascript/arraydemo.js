fb38010436ad7ce5b1df3b74399d50f645205b40
Bithe <farzana.hossain5@gmail.com>
2023-01-27 10:33:50 +0200

    array sum

--- Javascript/arraydemo.js
+++ Javascript/arraydemo.js
@@ -1,7 +1,19 @@
-let numbers, resultString='';
+// numbers.push(5);
+
+// let numbers, resultString='';
+// sum=0;
+// for(let i=0;i<4;i++){
+//     sum+=numbers[i];
+//     resultString+=numbers[i]+' + ';
+// }
+// console.log(resultString+' = '+sum);
+
+
+numbers.push(5);
+
 sum=0;
-for(let i=0;i<4;i++){
-    sum+=numbers[i];
-    resultString+=numbers[i]+' + ';
+for(let i=0;i<numbers.length;i++){
+    sum+= numbers[i];
 }
-console.log(resultString+' = '+sum);
+
+console.log(sum);
